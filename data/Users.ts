ec50053538abfd94154dca0b1cf4319bac0d0022
Salman Fazal <rustamas@uni.coventry.ac.uk>
2021-05-01 02:33:44 -0400

    Data

--- data/Users.ts
+++ data/Users.ts
@@ -8,56 +8,56 @@ export default [
   },
   {
     id: "u2",
-    name: "Lukas",
+    name: "Igor",
     imageUri:
       "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
     status: "Hello there, how are you",
   },
   {
     id: "u3",
-    name: "Daniil",
+    name: "Kumayl",
     imageUri:
       "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
   },
   {
     id: "u4",
-    name: "Alex",
+    name: "Ally",
     imageUri:
       "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
   },
   {
     id: "u5",
-    name: "Vlad",
+    name: "Tim",
     imageUri:
       "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
   },
   {
     id: "u6",
-    name: "Elon Musk",
+    name: "Elon",
     imageUri:
       "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
   },
   {
     id: "u7",
-    name: "Adrian",
+    name: "Brian",
     imageUri:
       "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
   },
   {
     id: "u8",
-    name: "Borja",
-    imageUri:
-      "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
-  },
-  {
-    id: "u9",
     name: "Mom",
     imageUri:
       "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
   },
   {
+    id: "u9",
+    name: "Dad",
+    imageUri:
+      "https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg",
+  },
+  {
     id: "u10",
-    name: "Angelina Jolie",
+    name: "Nati",
     imageUri:
       "https://lkbkspro.s3.amazonaws.com/atelier-management/gs_58d933b8-98b4-468e-b229-43100a9620a7.jpg",
   },
